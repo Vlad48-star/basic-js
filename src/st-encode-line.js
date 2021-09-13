b1c42155fe3560d53c503a53968e5fa6b18f8f9e
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2021-09-14 00:39:51 +0300

    add:create project

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let result = '';
+  let count = 0 ;
+  for(let i = 0; i < str.length; i++){
+      count += 1;
+      if(str[i] !== str[i+1]){
+          if(count === 1){
+              result = result + str[i];
+          } else{
+              result = result + count + str[i];
+          }  
+          count = 0;
+      }
+  }
+  return result
 }
+
--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -1,18 +1,13 @@
 import { NotImplementedError } from '../extensions/index.js';
 
-/**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
- *
- */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+    if(n<10){
+        return n
+    }
+    let sum = 0;
+    while (n != 0) {
+        sum = sum + n % 10;
+        n = parseInt(n / 10);
+    }
+    return getSumOfDigits(sum);
 }
